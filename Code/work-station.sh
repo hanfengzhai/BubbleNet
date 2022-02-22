36ae9e02efdf0e65b2d24158f8ef51a1784d94e7
Hanfeng Zhai <frankzhai0@gmail.com>
2022-02-21 21:10:37 -0500

    Update work-station.sh

--- Code/work-station.sh
+++ Code/work-station.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 python bubblesys_DNN.py # run it on traditional deep neural net
 python bubblesys_PhysNet.py # run it on BubbleNet
+python bubblesys_DNN.py # multiple bubble
+python bubblesys_PhysNet.py # single bubble
